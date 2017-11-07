6c80dadc66936d5fbf521648dd75d81d61de0d23
Nina Wahler <ninawahler@MacBook-Pro-de-Nina.local>
2017-11-07 22:43:45 +0100

    Initial Commit

---
+++ Brunel/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Brunel
+//
+//  Created by Nina Wahler on 07.11.17.
+//  Copyright © 2017 Nina Wahler. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
